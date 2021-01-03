ba960d2a79f2ed812c602c440a68b6ca31a0a23f
Ahmed ashraf <64101522+ahmedashrafgad@users.noreply.github.com>
2021-01-03 15:44:48 +0200

    Add files via upload

---
+++ StdTypes.h
@@ -0,0 +1,23 @@
+ /*
+ * StdTypes.h
+ *
+ * Created: 12/1/2020 11:21:41 AM
+ *  Author: Ahmed Ashraf
+ */ 
+
+
+#ifndef STDTYPES_H_
+#define STDTYPES_H_
+
+typedef unsigned char         u8;  //1 bytes
+typedef signed char          s8;  //1 bytes
+typedef unsigned short int   u16; //2 bytes
+typedef signed short int   s16; //2 bytes
+typedef unsigned long  int   u32; //4 bytes
+typedef signed long  int   s32; //4 bytes
+typedef unsigned long  long   u64; //8 bytes
+typedef signed long  long   s64; //8 bytes
+
+#define NULLPTR ((void*)0)
+
+#endif /* STDTYPES_H_ */
---
+++ utlis.h
@@ -0,0 +1,19 @@
+/*
+ * utlis.h
+ *
+ * Created: 12/1/2020 11:20:27 AM
+ *  Author: Ahmed Ashraf
+ */ 
+
+
+#ifndef UTLIS_H_
+#define UTLIS_H_
+
+
+#define SET_BIT(VAR,BIT)          VAR |=  (1 << (BIT))
+#define CLR_BIT(VAR,BIT)          VAR &= ~(1 << (BIT))
+#define GET_BIT(VAR,BIT)          ((VAR >> BIT) & 1  )
+#define TOG_BIT(VAR,BIT)          VAR ^=  (1 << (BIT))
+
+
+#endif /* UTLIS_H_ */
